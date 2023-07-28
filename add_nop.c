d0b30af84dd2975044492bbf6435c6d781683c1c
Khansmira <miriamakanwa@gmail.com>
2023-07-27 18:02:17 -0700

    add and nop implementation

---
+++ add_nop.c
@@ -0,0 +1,39 @@
+#include <ctype.h>
+#include <string.h>
+#include "monty.h"
+
+/**
+ * to_add -  adds the top two nodes of the stack
+ * @stack: stack double pointer
+ * @count: line number of opcode
+ *
+ * Return: void
+ */
+void _add(stack_t **stack, unsigned int count)
+{
+	int s;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", count;
+		exit(EXIT_FAILURE);
+	}
+
+	s = ((*stack)->next->n) + ((*stack)->n);
+	(*stack)->n = s;
+	pop(stack, count);
+}
+
+/**
+ * nop - this does nothing
+ * @stack: void
+ * @count : void
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int count)
+{
+	(void) stack;
+	(void) count;
+}
+
